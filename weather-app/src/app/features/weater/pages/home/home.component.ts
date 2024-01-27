0c8c26dda56c3fd52c73f8b040bff03174142110
Daniel-BS-Dev <danielbenedito263@gmail.com>
2024-01-27 16:30:19 -0300

    Consuning weather service

--- weather-app/src/app/features/weater/pages/home/home.component.ts
+++ weather-app/src/app/features/weater/pages/home/home.component.ts
@@ -1,16 +1,36 @@
 import { Component } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
+import { HttpClientModule } from '@angular/common/http';
+import { WeatherModel } from '../../../model/weather';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   providers: [WeatherService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  initialCityName = 'São Paulo';
+  weatherData!: WeatherModel;
+
   constructor(private service: WeatherService) { }
+
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+  getWeatherDatas(cityName: string) {
+    this.service.getWeatherDatas(cityName)
+      .subscribe({
+        next: (response) => {
+          response && (this.weatherData = response);
+        },
+        error: (error) => console.log(error)
+      });
+
+  }
 
 }
