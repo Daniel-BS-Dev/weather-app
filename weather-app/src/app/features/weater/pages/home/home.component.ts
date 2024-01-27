e2dcf90e81e6bfd0e27efc39b75e6a164355731f
Daniel-BS-Dev <danielbenedito263@gmail.com>
2024-01-27 15:08:21 -0300

    Creat weather service

--- weather-app/src/app/features/weater/pages/home/home.component.ts
+++ weather-app/src/app/features/weater/pages/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
+  providers: [WeatherService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor(private service: WeatherService) { }
 
 }
