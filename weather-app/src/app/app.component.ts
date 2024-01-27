b4ada432e313684f26f09c304f7b97e46301b635
Daniel-BS-Dev <danielbenedito263@gmail.com>
2024-01-27 14:38:28 -0300

    Creat home Component

--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from './features/weater/pages/home/home.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -5,7 +6,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule],
+  imports: [RouterOutlet, FontAwesomeModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- weather-app/src/app/app.routes.ts
+++ weather-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/weater/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'weather',
+    pathMatch: 'full',
+  },
+  {
+    path: 'weather',
+    component: HomeComponent,
+  }
+];
