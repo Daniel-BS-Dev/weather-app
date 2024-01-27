134a8238100577d0294748d1dcc235ab3a8116c2
Daniel-BS-Dev <danielbenedito263@gmail.com>
2024-01-27 16:56:06 -0300

    Add search field

--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { HomeComponent } from './features/weater/pages/home/home.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule, HomeComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- weather-app/src/app/features/weater/pages/home/home.component.ts
+++ weather-app/src/app/features/weater/pages/home/home.component.ts
@@ -2,24 +2,33 @@ import { Component } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { HttpClientModule } from '@angular/common/http';
 import { WeatherModel } from '../../../model/weather';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, FontAwesomeModule, ReactiveFormsModule],
   providers: [WeatherService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
-  initialCityName = 'São Paulo';
+  initialCityName = new FormControl('');
   weatherData!: WeatherModel;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private service: WeatherService) { }
 
   ngOnInit(): void {
-    this.getWeatherDatas(this.initialCityName);
+    this.getWeatherDatas(this.initialCityName.value || '');
+  }
+
+  onSubmit() {
+    this.getWeatherDatas(this.initialCityName.value || '');
+    this.initialCityName.patchValue('');
   }
 
   getWeatherDatas(cityName: string) {
